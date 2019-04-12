c626d374e4d1da6de25483a3ab9c01e131283683
David Haugen <haugen.dj@gmail.com>
2019-04-12 16:33:54 -0500

    update landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -6,7 +6,7 @@ export default class LandingPage extends Component {
         return (
             <div className="LandingPage">
                 <div className="welcome">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p>Thanks for coming to Petful! You can only adopt the next dog or cat in line, no jumping ahead! If you miss your turn, don't worry, everyone cycles through every 3 seconds.</p>
                 </div>
                 <button><Link to="/pets">Start</Link></button>
             </div>
